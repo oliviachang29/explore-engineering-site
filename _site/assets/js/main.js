ccd7457cc7bbf6a430afcf1b5f017cc4f479d5ca
Olivia <cozymonster29@gmail.com>
2019-04-14 16:26:45 -0700

    add speakers section, lazy loading, and smoothscroll

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,10 +1,13 @@
 $(function() {
 	// $('body').scrollspy({ target: '#navbar' })
-		$(document).scroll(function() {
-			$nav = $(".navbar");
-			$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-			$nav.toggleClass('navbar-dark', $(this).scrollTop() < $nav.height());
-			$nav.toggleClass('navbar-light', $(this).scrollTop() > $nav.height());
-			
-		});
+	$(document).scroll(function() {
+		$nav = $(".navbar");
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+		$nav.toggleClass('navbar-dark', $(this).scrollTop() < $nav.height());
+		$nav.toggleClass('navbar-light', $(this).scrollTop() > $nav.height());
+		
+	});
+
+	var scroll = new SmoothScroll('a[href*="#"]');
+    var lazyLoadInstance = new LazyLoad();
 });
